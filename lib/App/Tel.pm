e7ff720def240c0b198d28e5716328ecd931fffe
Jan Egil Vestbø <jan.egil.vestbo@avinor.no>
2022-02-24 20:47:29 +0100

    Fixes both enabled an not-enabled devices

--- lib/App/Tel.pm
+++ lib/App/Tel.pm
@@ -563,16 +563,16 @@ sub enable {
     my $profile = $self->profile;
 
     if ($profile->{enablecmd}) {
-        $self->send($profile->{enablecmd} . "\r");
-
         $profile->{ena_username_prompt} ||= qr/[Uu]ser[Nn]ame:|Login:/;
         $profile->{ena_password_prompt} ||= qr/[Pp]ass[Ww]ord/;
-
+        
         # we need to be able to handle routers that prompt for username and password
         # need to check the results to see if enable succeeded
         $self->expect($self->{timeout},
                 [ $profile->{ena_username_prompt} => sub { $self->send("$profile->{user}\r"); exp_continue; } ],
-                [ $profile->{ena_password_prompt} => sub { $self->send($self->password('enable') . "\r"); } ]
+                [ $profile->{ena_password_prompt} => sub { $self->send($self->password('enable') . "\r"); } ],
+                [ $profile->{prompt} => sub { $self->{enabled}=1; $self->send("\r"); } ],
+                [ $profile->{disabledprompt} => sub { $self->send("$profile->{enablecmd}\r"); exp_continue; } ]
         );
     }
 
